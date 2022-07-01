2043c401b7c0b2a139fe12a08f30c58059048ee7
geyao-c <1279846803@qq.com>
2022-07-01 09:22:49 +0800

    xiaoxiu

--- WGAN_new.py
+++ WGAN_new.py
@@ -244,8 +244,9 @@ if __name__ == '__main__':
             gen_iterations += 1
 
             # 生成图片
-            if total_DG % 5000 == 0:
-                real_cpu = data[0].mul(0.5).add(0.5)
+            if total_DG % 500 == 0:
+                print('save image')
+                real_cpu = data.mul(0.5).add(0.5)
                 real_image_path = os.path.join(image_generate_dir, '{}_real_samples.png'.format(total_DG))
                 vutils.save_image(real_cpu, real_image_path)
 
